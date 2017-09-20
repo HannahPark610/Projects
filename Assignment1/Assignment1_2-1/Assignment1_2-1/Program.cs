e53cb00f6e296c497778124003c0cb7b91b3770f
Hannah Park <teyang610@gmail.com>
2017-09-20 12:53:03 -0700

    Study C#

---
+++ Assignment1/Assignment1_2-1/Assignment1_2-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Assignment1_21
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hannah");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Operators/2.IfElse/2.IfElse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfElse
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            if (first == second)
+            {
+                Console.WriteLine("both are equal");
+            }
+            else if (first > second)
+            {
+                Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("second number is ");
+                }
+                Console.WriteLine("both are not equal");
+            }
+            Console.Read();
+        }
+    }
+
