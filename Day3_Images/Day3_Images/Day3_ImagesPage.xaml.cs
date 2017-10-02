bd6ac11dcec84af5cfbb5a72ee26ebccc285d23f
Hannah Park <teyang610@gmail.com>
2017-10-02 11:07:29 -0700

    Day3_Images

---
+++ Day3_Images/Day3_Images/Day3_ImagesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Day3_Images
+{
+    public partial class Day3_ImagesPage : ContentPage
+    {
+        public Day3_ImagesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Day3_Images/Day3_Images/PlatformSpecificImage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Day3_Images
+{
+    public partial class PlatformSpecificImage : ContentPage
+    {
+        public PlatformSpecificImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
