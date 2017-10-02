7b0c4f578ed711452456d067cf1133b614cfd482
Hannah Park <teyang610@gmail.com>
2017-10-02 10:38:33 -0700

    Day2_Layout

---
+++ Day2_Layout/Day2_Layout/Day2_LayoutPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Day2_Layout
+{
+    public partial class Day2_LayoutPage : ContentPage
+    {
+        public Day2_LayoutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
